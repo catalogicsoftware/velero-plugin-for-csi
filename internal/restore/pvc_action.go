58471f6d4c51544113f39128a401249b8c359d12
Swanand Shende <sshende@catalogicsoftware.com>
2021-07-16 09:09:02 +0530

    Merge latest changes from Velero repository

--- internal/restore/pvc_action.go
+++ internal/restore/pvc_action.go
@@ -31,6 +31,16 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/vmware-tanzu/velero-plugin-for-csi/internal/util"
+	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
+)
+
+const (
+	AnnBindCompleted      = "pv.kubernetes.io/bind-completed"
+	AnnBoundByController  = "pv.kubernetes.io/bound-by-controller"
+	AnnStorageProvisioner = "volume.beta.kubernetes.io/storage-provisioner"
+	AnnSelectedNode       = "volume.kubernetes.io/selected-node"
 )
 
 // PVCRestoreItemAction is a restore item action plugin for Velero
@@ -46,13 +56,13 @@ func (p *PVCRestoreItemAction) AppliesTo() (velero.ResourceSelector, error) {
 	}, nil
 }
 
-func resetPVCAnnotations(pvc *corev1api.PersistentVolumeClaim, preserve []string) {
+func removePVCAnnotations(pvc *corev1api.PersistentVolumeClaim, remove []string) {
 	if pvc.Annotations == nil {
 		pvc.Annotations = make(map[string]string)
 		return
 	}
 	for k := range pvc.Annotations {
-		if !util.Contains(preserve, k) {
+		if util.Contains(remove, k) {
 			delete(pvc.Annotations, k)
 		}
 	}
@@ -105,7 +115,14 @@ func (p *PVCRestoreItemAction) Execute(input *velero.RestoreItemActionExecuteInp
 
 	p.Log.Infof("Starting PVCRestoreItemAction for PVC %s/%s", pvc.Namespace, pvc.Name)
 
-	resetPVCAnnotations(&pvc, []string{velerov1api.BackupNameLabel, util.VolumeSnapshotLabel})
+	removePVCAnnotations(&pvc,
+		[]string{AnnBindCompleted, AnnBoundByController, AnnStorageProvisioner, AnnSelectedNode})
+
+	// If cross-namespace restore is configured, change the namespace
+	// for PVC object to be restored
+	if val, ok := input.Restore.Spec.NamespaceMapping[pvc.GetNamespace()]; ok {
+		pvc.SetNamespace(val)
+	}
 
 	if input.Restore.Spec.RestorePVs != nil && *input.Restore.Spec.RestorePVs == false {
 		p.Log.Info("Returning from PersistentVolumeClaimRestoreItemAction as restorePVs flag is set to false")
